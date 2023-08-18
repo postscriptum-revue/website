1c18b009058a9413f4ec2e156b3e625b7edee1ae
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-08-17 20:30:24 -0400

    integrate aside + header

---
+++ site/snippets/logo.php
@@ -0,0 +1,17 @@
+<?php
+
+// TODO: Replace with real logic.
+$p_ss = random_int(0, 1) . random_int(0, 9);
+$s_ss = random_int(0, 1) . random_int(0, 9);
+
+?>
+
+<a href="/" class="site-header__logo">
+	<?php if ($page->template() == "home"): ?>
+	<span
+		style="font-feature-settings: 'ss<?= $p_ss ?>';">P</span>OST<span
+		style="font-feature-settings: 'ss<?= $s_ss ?>'">S</span>CRIPTUM
+	<?php else: ?>
+	PS
+	<?php endif ?>
+</a>
