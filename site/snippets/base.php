ba223b40535ff31c9261228d97549ca643220392
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-08-18 20:22:04 -0400

    format php

--- site/snippets/base.php
+++ site/snippets/base.php
@@ -7,12 +7,12 @@
 	<link rel="stylesheet" href="/assets/css/style.css">
 	<title>
 		<?php
-            if ($page->template() == "home") {
-                echo "Post-Scriptum";
-            } else {
-                // TODO: Display "PS12" when in an issue or an article.
-                echo "PS";
-            }
+		if ($page->template() == "home") {
+			echo "Post-Scriptum";
+		} else {
+			// TODO: Display "PS12" when in an issue or an article.
+			echo "PS";
+		}
 		?>
 	</title>
 </head>
