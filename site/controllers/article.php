267af423df292b53f137c62edc3820dfe7248515
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-08-27 22:41:13 -0400

    save
    save

---
+++ site/controllers/article.php
@@ -0,0 +1,9 @@
+<?php
+
+return function ($page) {
+	$issue_color = $page->parent()->color();
+
+	return [
+		"issue_color" => $issue_color
+	];
+};
--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -1,3 +1,7 @@
-<figure class="text-block__figure">
-	<img src="<?= $block->image()->toFile()->url() ?>" alt="">
+<figure class="image-block">
+	<!-- TODO: Add alt -->
+	<img class="image-block__image" src="<?= $block->image()->toFile()->url() ?>" alt="">
+	<?php if ($block->caption()) : ?>
+		<figcaption class="image-block__caption"><?= $block->caption()->widont() ?></figcaption>
+	<?php endif ?>
 </figure>
