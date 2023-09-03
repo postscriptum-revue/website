ecc79ace79d637ef7e7bd2905873344a02c36d2b
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-09-03 12:14:49 -0400

    add articles and keywords logic

--- site/templates/articles.php
+++ site/templates/articles.php
@@ -1,21 +1,32 @@
 <?php snippet("site/layout", slots: true) ?>
 
 <?php slot("aside") ?>
-
+<section class="site-aside__section">
+	<h2 class="site-aside__title">Mots-clés</h2>
+	<ul class="articles-aside-list">
+		<?php foreach ($keywords as $kw) : ?>
+			<li class="articles-aside-list__item">
+				<a href="<?= $page->url(['params' => ['keyword' => $kw]]) ?>"><?= $kw ?></a>
+			</li>
+		<?php endforeach ?>
+	</ul>
+</section>
 <?php endslot() ?>
 
 <?php slot("main") ?>
 <?php foreach ($articles as $article) : ?>
-	<article class="article">
-		<ul class="article__authors">
-			<?php foreach ($article->authors()->toStructure() as $author) : ?>
-				<li><?= $author->name() ?></li>
-			<?php endforeach ?>
-		</ul>
-		<hgroup class="article__hgroup">
-			<h2 class="article__title"><?= $article->title() ?></h2>
-			<p class="article__subtitle"><?= $article->subtitle() ?></p>
-		</hgroup>
-	</article>
+	<a href="<?= $article->url() ?>" class="article">
+		<article>
+			<ul class="article__authors">
+				<?php foreach ($article->authors()->toStructure() as $author) : ?>
+					<li><?= $author->name() ?></li>
+				<?php endforeach ?>
+			</ul>
+			<hgroup class="article__hgroup">
+				<h2 class="article__title"><?= $article->title() ?></h2>
+				<p class="article__subtitle"><?= $article->subtitle() ?></p>
+			</hgroup>
+		</article>
+	</a>
 <?php endforeach ?>
 <?php endslot() ?>
