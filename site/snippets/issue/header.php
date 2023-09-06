008b55ad010335fb589853e1916493df9fc697d8
Raphych <117363314+Raphych@users.noreply.github.com>
2023-09-06 14:55:24 -0400

    Modification du php des crédits: n'affiche un bloc de crédit que lorsque son champ est rempli.

--- site/snippets/issue/header.php
+++ site/snippets/issue/header.php
@@ -1,6 +1,6 @@
 <header class="issue-header">
 	<figure class="issue-header__cover">
-		<img class="issue-header__cover-image" src="<?php if ($page->cover()) echo $page->cover()->toFile()->url() ?>" alt="">
+		<img class="issue-header__cover-image" src="<?php if ($cover = $page->cover()->toFile()) echo $cover->url() ?>" alt="">
 		<figcaption><!-- TODO: Add figcaption field --></figcaption>
 	</figure>
 	<div class="issue-header__card-wrapper">
