66211186377c861c4ca903b48f6e3cd515f4c2fa
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-09-07 13:10:22 -0400

    clean template code

--- site/snippets/site/header.php
+++ site/snippets/site/header.php
@@ -3,12 +3,36 @@
 	<nav class="site-header__nav">
 		<!-- TODO: Generate w/ PHP -->
 		<ul class="site-header__nav-list">
-			<li><a class="site-header__nav-list-item" href="/parutions">Parutions</a></li>
-			<li><a class="site-header__nav-list-item" href="/articles">Articles</a></li>
-			<li><a class="site-header__nav-list-item" href="/">Comptes-rendus</a></li>
-			<li><a class="site-header__nav-list-item" href="/">Entretiens</a></li>
-			<li><a class="site-header__nav-list-item" href="/">Créations</a></li>
-			<li><a class="site-header__nav-list-item" href="/">À propos</a></li>
+			<li>
+				<a class="site-header__nav-list-item" href="/parutions">
+					Parutions
+				</a>
+			</li>
+			<li>
+				<a class="site-header__nav-list-item" href="/articles">
+					Articles
+				</a>
+			</li>
+			<li>
+				<a class="site-header__nav-list-item" href="/">
+					Comptes-rendus
+				</a>
+			</li>
+			<li>
+				<a class="site-header__nav-list-item" href="/">
+					Entretiens
+				</a>
+			</li>
+			<li>
+				<a class="site-header__nav-list-item" href="/">
+					Créations
+				</a>
+			</li>
+			<li>
+				<a class="site-header__nav-list-item" href="/">
+					À propos
+				</a>
+			</li>
 		</ul>
 	</nav>
 </header>
--- site/templates/issue.php
+++ site/templates/issue.php
@@ -5,32 +5,50 @@
 <?php endslot() ?>
 
 <?php slot("main") ?>
+
 <?php snippet("issue/header") ?>
+
 <section class="text-section issue-text">
 	<hgroup class="text-section-hgroup">
 		<?php if ($page->intro_title()->isNotEmpty()) : ?>
 			<p>Texte de présentation</p>
-			<h2 class="text-section-hgroup__title"><?= $page->intro_title() ?></h2>
+			<h2 class="text-section-hgroup__title">
+				<?= $page->intro_title() ?>
+			</h2>
 			<?php if ($page->intro_subtitle()) : ?>
 				<p><?= $page->intro_subtitle() ?></p>
 			<?php endif ?>
 		<?php else : ?>
-			<h2 class="text-section-hgroup__title">Texte de présentation</h2>
+			<h2 class="text-section-hgroup__title">
+				Texte de présentation
+			</h2>
 		<?php endif ?>
 	</hgroup>
 	<?= $page->intro_text()->toBlocks() ?>
 </section>
+
 <dl class="issue-credits">
 	<!-- TODO: Normalize terms. -->
 	<dt class="issue-credits__term">Image de couverture</dt>
-	<dd class="issue-credits__description"><? if ($page->cover()) echo $page->cover()->toFile()->credit() ?></dd>
+	<dd class="issue-credits__description">
+		<?= $page->cover()->toFile()->credit() ?>
+	</dd>
 	<dt class="issue-credits__term">Texte de présentation</dt>
-	<dd class="issue-credits__description"><?= $page->credit_intro() ?></dd>
+	<dd class="issue-credits__description">
+		<?= $page->credit_intro() ?>
+	</dd>
 	<dt class="issue-credits__term">Éditeur·rice(s)</dt>
-	<dd class="issue-credits__description"><?= $page->credit_editors() ?></dd>
+	<dd class="issue-credits__description">
+		<?= $page->credit_editors() ?>
+	</dd>
 	<dt class="issue-credits__term">Révision</dt>
-	<dd class="issue-credits__description"><?= $page->credit_revision() ?></dd>
+	<dd class="issue-credits__description">
+		<?= $page->credit_revision() ?>
+	</dd>
 	<dt class="issue-credits__term">Mise en ligne</dt>
-	<dd class="issue-credits__description"><?= $page->credit_webmaster() ?></dd>
+	<dd class="issue-credits__description">
+		<?= $page->credit_webmaster() ?>
+	</dd>
 </dl>
+
 <?php endslot() ?>
