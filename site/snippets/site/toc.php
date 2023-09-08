1ca03f11f17e3fdea63654a2233bf43c1f72fdbd
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-09-08 10:13:37 -0400

    clean templates

--- site/snippets/site/toc.php
+++ site/snippets/site/toc.php
@@ -4,11 +4,20 @@
 			<a href="<?= $toc_page->url() ?>">
 				<article>
 					<ul class="toc__item-authors">
-						<?php foreach ($toc_page->authors()->toStructure() as $author) : ?>
+						<?php
+						foreach ($toc_page->authors()->toStructure()
+							as $author) : ?>
 							<li><?= $author->name() ?></li>
 						<?php endforeach ?>
 					</ul>
-					<p class="toc__item-title"><?= $toc_page->title() ?> : <?= $toc_page->subtitle() ?></p>
+					<p class="toc__item-title">
+						<?= $toc_page->title() ?>
+					</p>
+					<?php if ($toc_page->subtitle() != "") : ?>
+						<p class="toc__item-subtitle">
+							<?= $toc_page->subtitle() ?>
+						</p>
+					<?php endif ?>
 				</article>
 			</a>
 		</li>
