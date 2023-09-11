669dc0be4d46b8da4e91a8f2c51e2edb31574995
Raphych <117363314+Raphych@users.noreply.github.com>
2023-09-11 10:14:49 -0400

    Correction et ajout du champ de texte alternatif.

--- site/snippets/issue/header.php
+++ site/snippets/issue/header.php
@@ -1,6 +1,6 @@
 <header class="issue-header">
 	<figure class="issue-header__cover">
-		<img class="issue-header__cover-image" src="<?= $page->cover()->toFile->url() ?>" alt="">
+		<img class="issue-header__cover-image" src="<?= $page->cover()->toFile()->url() ?>" alt="<?= $page->cover()->toFile()->alt_text() ?>">
 		<figcaption><!-- TODO: Add figcaption field --></figcaption>
 	</figure>
 	<div class="issue-header__card-wrapper">
