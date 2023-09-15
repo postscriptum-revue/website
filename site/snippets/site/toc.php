c52f5475d497fb458128e2b82ee64a65964bbe37
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-09-15 10:42:27 -0400

    Simplifier le balisage de toc

--- site/snippets/site/toc.php
+++ site/snippets/site/toc.php
@@ -2,21 +2,19 @@
 	<?php foreach ($toc_pages as $toc_page) : ?>
 		<li class="toc__item">
 			<a href="<?= $toc_page->url() ?>">
-				<article>
-					<ul class="toc__item-authors">
-						<?php
-						foreach ($toc_page->authors()->toStructure()
-							as $author) : ?>
-							<li><?= $author->name() ?></li>
-						<?php endforeach ?>
-					</ul>
-					<p class="toc__item-title"><?= $toc_page->title() ?></p>
-					<?php if ($toc_page->subtitle() != "") : ?>
-						<p class="toc__item-subtitle">
-							<?= $toc_page->subtitle() ?>
-						</p>
-					<?php endif ?>
-				</article>
+				<ul class="toc__item-authors">
+					<?php
+					foreach ($toc_page->authors()->toStructure()
+						as $author) : ?>
+						<li><?= $author->name() ?></li>
+					<?php endforeach ?>
+				</ul>
+				<p class="toc__item-title"><?= $toc_page->title() ?></p>
+				<?php if ($toc_page->subtitle() != "") : ?>
+					<p class="toc__item-subtitle">
+						<?= $toc_page->subtitle() ?>
+					</p>
+				<?php endif ?>
 			</a>
 		</li>
 	<?php endforeach ?>
