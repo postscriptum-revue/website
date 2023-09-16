f0e3ea3b9a57684e9275da4a69991ce8b70cab01
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-09-16 19:19:54 -0400

    Initial work for news section

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,21 @@
 <?php snippet("site/layout", slots: true) ?>
 
 <?php slot("aside") ?>
-
+<h2 class="site-aside__title">Actualités</h2>
+<ul>
+	<?php foreach ($all_news as $news_item) : ?>
+		<li>
+			<article class="home-aside__news-item">
+				<!-- TODO: Alt text -->
+				<!-- <figure class="home-aside__news-item-img">
+					<img src="<?= $news_item->issue_cover()->toFile()->url() ?>" alt="">
+				</figure> -->
+				<p><?= $news_item->parent()->title() ?></p>
+				<p><?= $news_item->title() ?></p>
+			</article>
+		</li>
+	<?php endforeach ?>
+</ul>
 <?php endslot() ?>
 
 <?php slot("main") ?>
