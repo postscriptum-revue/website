88eb205c0724267d444fee0fadbef745eb6c2867
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-09-16 19:19:54 -0400

    Initial work for news section

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -1,11 +1,20 @@
 <?php
 
 return function () {
+
+	$news_page = page("actualites");
+	$all_news = new Pages();
+
+	foreach ($news_page->children() as $news_section) {
+		$all_news->add($news_section->children());
+	}
+
 	$last_issue = page("parutions")->children()->last();
 	$latest_issues = page("parutions")->children()->listed()->flip()->slice(1, 3);
 
 	return [
 		"last_issue" => $last_issue,
-		"latest_issues" => $latest_issues
+		"latest_issues" => $latest_issues,
+		"all_news" => $all_news
 	];
 };
