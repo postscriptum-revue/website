8b0382f14f163b9d636d586b7ae85f4b4a28cc99
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-09-20 21:31:16 -0400

    Use TOC snippet for articles page

--- site/templates/articles.php
+++ site/templates/articles.php
@@ -18,26 +18,5 @@
 <?php endslot() ?>
 
 <?php slot("main") ?>
-<?php foreach ($articles as $article) : ?>
-	<a href="<?= $article->url() ?>" class="article">
-		<article>
-			<ul class="article__authors">
-				<?php
-				foreach ($article->authors()->toStructure()
-					as $author) :
-				?>
-					<li><?= $author->name() ?></li>
-				<?php endforeach ?>
-			</ul>
-			<hgroup class="article__hgroup">
-				<h2 class="article__title">
-					<?= $article->title() ?>
-				</h2>
-				<p class="article__subtitle">
-					<?= $article->subtitle() ?>
-				</p>
-			</hgroup>
-		</article>
-	</a>
-<?php endforeach ?>
+<?php snippet("site/toc", ["toc_pages" => $articles]) ?>
 <?php endslot() ?>
