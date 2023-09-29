0dabd17c28403014a06cfaa4cd2cc461f040d9a1
Maxime Pigeon <37006580+maxime-pigeon@users.noreply.github.com>
2023-09-29 19:24:22 -0400

    Add template for design guidelines

---
+++ site/templates/guidelines.php
@@ -0,0 +1,27 @@
+<?php snippet("site/layout", slots: true) ?>
+
+<?php slot("aside") ?>
+
+<h2 class="site-aside__title">Logo</h2>
+<ul>
+	<?php foreach (page("parutions")->children()->flip() as $issue) : ?>
+		<li>
+			<details class="details">
+				<summary class="summary">
+					PS<?= $issue->num() ?>
+				</summary>
+				<!-- TODO: Remove if -->
+				<?php if ($issue->file("logo-black.pdf")) : ?>
+					<a href="<?= $issue->file("logo-black.pdf")->url() ?>">
+						logo-black.pdf
+					</a>
+				<?php endif ?>
+			</details>
+		</li>
+	<?php endforeach ?>
+</ul>
+<?php endslot() ?>
+
+<?php slot("main") ?>
+
+<?php endslot() ?>
