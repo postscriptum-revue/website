a747a504e0ca530ddab136bd197f4aa04d599aa3
Maxime Pigeon <me@maximepigeon.com>
2023-10-20 10:39:36 -0400

    Ajouter la logique pour le logo des petites couvertures

--- site/snippets/site/issue-small.php
+++ site/snippets/site/issue-small.php
@@ -5,14 +5,29 @@
 	>
 		<div 
 			class="latest-issues__issue-cover" 
-			style="background-image: url('<?= $issue->cover()->toFile()->url() ?>')">
+			style="background-image: url(
+				'<?= $issue->cover()->toFile()->url() ?>'
+			)"
+		>
 			<div class="latest-issues__issue-card">
-				<p class="latest-issues__issue-card-number">PS<?= $issue->num() ?></p>
+				<p class="latest-issues__issue-card-number">
+					<span style="
+						font-feature-settings: 
+							'ss<?= $issue->logo_style_p() ?>';
+					">P</span><span style="
+						font-feature-settings: 
+							'ss<?= $issue->logo_style_s() ?>';
+					">S</span><?= $issue->num() ?>
+				</p>
 			</div>
 		</div>
 		<hgroup class="latest-issues__issue-hgroup">
-			<time class="latest-issues__issue-date"><?= $issue->date() ?></time>
-			<h3 class="latest-issues__issue-title"><?= $issue->title() ?></h3>
+			<time class="latest-issues__issue-date">
+				<?= $issue->date() ?>
+			</time>
+			<h3 class="latest-issues__issue-title">
+				<?= $issue->title() ?>
+			</h3>
 		</hgroup>
 	</article>
 </a>
