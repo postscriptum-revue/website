83cfe1561952eeaf347148bce3d965bb42ea04cb
Maxime Pigeon <me@maximepigeon.com>
2023-10-22 11:42:31 -0400

    Ajouter logique de base pour couleur

--- site/plugins/issue-color/index.php
+++ site/plugins/issue-color/index.php
@@ -1,13 +1,23 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+use ColorThief\ColorThief;
+
 \Kirby\Cms\App::plugin("postscriptum/issue-color", [
 	"hooks" => [
-		// Make sure it works when file are changed
+		// TODO Make sure it works when file are changed
 		"file.create:after" => function (Kirby\Cms\File $file) {
 			if ($file->template() == "cover") {
-
+				$dominant_color = ColorThief::getColor(
+					$file->root(),
+					outputFormat: "hex"
+				);
+				file_put_contents(__DIR__ . "/log.txt", $dominant_color);
+				$file->page()->update([
+					"color" => $dominant_color
+				]);
 			}
-			// file_put_contents(__DIR__ . "/log.txt", $file->template());
 		}
 	]
 ]);
