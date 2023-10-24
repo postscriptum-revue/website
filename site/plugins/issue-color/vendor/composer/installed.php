397353bf91749b23f7fe56d315332d018ce83d9b
Maxime Pigeon <me@maximepigeon.com>
2023-10-24 16:27:00 -0400

    Modifier logique pour prendre en compte le contraste

---
+++ site/plugins/issue-color/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'postscriptum/issue-color',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2a21cd6a2159f87118550552cb3526b2d4ff86c5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ksubileau/color-thief-php' => array(
+            'pretty_version' => 'v2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => 'a1378533433dae824c2e5b70359f8b6ae16b1deb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ksubileau/color-thief-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'postscriptum/issue-color' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2a21cd6a2159f87118550552cb3526b2d4ff86c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
