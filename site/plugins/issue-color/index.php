2a748fd39f3bce8e5cbafbcacae136c6e11a37b9
Maxime Pigeon <me@maximepigeon.com>
2023-10-24 16:27:00 -0400

    Modifier logique pour prendre en compte le contraste

--- site/plugins/issue-color/index.php
+++ site/plugins/issue-color/index.php
@@ -6,18 +6,67 @@ use ColorThief\ColorThief;
 
 \Kirby\Cms\App::plugin("postscriptum/issue-color", [
 	"hooks" => [
-		// TODO Make sure it works when file are changed
+		// TODO : Right now, this is only triggered when files
+		// are uploaded. There doesn't seem to be a hook for a
+		// file is selected in the panel.
 		"file.create:after" => function (Kirby\Cms\File $file) {
 			if ($file->template() == "cover") {
-				$dominant_color = ColorThief::getColor(
-					$file->root(),
-					outputFormat: "hex"
-				);
-				file_put_contents(__DIR__ . "/log.txt", $dominant_color);
+				$color = findIssueColor($file);
+
 				$file->page()->update([
-					"color" => $dominant_color
+					"color" => "rgb($color[0], $color[1], $color[2])",
 				]);
+
+				throw new Exception('Create');
 			}
 		}
 	]
 ]);
+
+function findIssueColor($file)
+{
+	$color = ColorThief::getColor(
+		$file->root()
+	);
+
+	$contrast = findContrast($color[0], $color[1], $color[2]);
+	$minContrast = 4.1;
+
+	while ($contrast < $minContrast) {
+		foreach ($color as &$channel) {
+			$channel < 5 ? $channel = 0 : $channel -= 5;
+		}
+
+		$contrast = findContrast($color[0], $color[1], $color[2]);
+	}
+
+	return $color;
+}
+
+function findLuminance($r, $g, $b)
+{
+	return 0.2126 *  linearize($r) +
+		0.7152 *  linearize($g) +
+		0.0722 *  linearize($b);
+}
+
+function linearize($channel)
+{
+
+	if ($channel <= 0.04045) {
+		return $channel / 12.92;
+	} else {
+		return pow((($channel + 0.055) / 1.055), 2.4);
+	}
+}
+
+function findContrast($r, $g, $b)
+{
+	$r /= 255;
+	$g /= 255;
+	$b /= 255;
+
+	$l = findLuminance($r, $g, $b);
+
+	return (1.0 + 0.05) / ($l + 0.05);
+}
