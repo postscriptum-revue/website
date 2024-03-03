5a191b4d8843e0eb75f57d40b6e74f3afb1f92b6
Maxime Pigeon <me@maximepigeon.com>
2024-03-03 15:56:35 -0500

    Add interviews page

---
+++ site/models/interview.php
@@ -0,0 +1,29 @@
+<?php
+
+use Kirby\Cms\Page;
+
+class InterviewPage extends Page
+{
+	public function title_and_subtitle()
+	{
+		$title = parent::title();
+		if (parent::formatted_title()->isNotEmpty()) {
+			$title = parent::formatted_title();
+		}
+
+		if (
+			!str_ends_with($title, "?")
+			&& !str_ends_with($title, "!")
+			&& !str_ends_with($title, ".")
+			&& parent::subtitle() != ""
+		) {
+			$title .= "&nbsp;: ";
+		}
+
+		if (parent::subtitle() != "") {
+			$title .= parent::subtitle();
+		}
+
+		return $title;
+	}
+}
