27c47b6dfabab1a2408dabcd589d738014bb091b
Maxime Pigeon <me@maximepigeon.com>
2024-04-18 14:58:00 -0400

    Add news to homepage + call for papers template

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,28 +3,7 @@
 <?php slot("aside_button") ?>Actualités<?php endslot() ?>
 
 <?php slot("aside") ?>
-
-<h2 class="site-aside__title">Actualités</h2>
-<ul>
-	<?php foreach ($all_news as $n) : ?>
-		<li class="home-aside__news-item">
-			<!-- TODO: Placeholder code. Use real date. -->
-			<date>
-				<?= substr_replace(
-					substr_replace($n->num(), "/", -2, 0), "/", 4, 0
-				) ?>
-			</date>
-			<p><?= $n->parent()->title() ?></p>
-			<p>
-				PS<?= $n->issue_number() ?>.
-				<?= $n->issue_title()->smartypants() ?>
-				<?php if ($n->issue_subtitle() != "") : ?>
-					<?= $n->issue_subtitle() ?>
-				<?php endif ?>
-			</p>
-		</li>
-	<?php endforeach ?>
-</ul>
+<?php snippet("home/news") ?>
 <?php endslot() ?>
 
 <?php slot("main") ?>
