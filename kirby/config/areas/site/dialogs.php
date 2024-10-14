ae4009d7834bdfe52d9b174417fbf1b77e84b373
Maxime Pigeon <me@maximepigeon.com>
2024-10-14 14:43:26 -0400

    Update Kirby

--- kirby/config/areas/site/dialogs.php
+++ kirby/config/areas/site/dialogs.php
@@ -10,6 +10,7 @@ use Kirby\Panel\ChangesDialog;
 use Kirby\Panel\Field;
 use Kirby\Panel\PageCreateDialog;
 use Kirby\Panel\Panel;
+use Kirby\Toolkit\Escape;
 use Kirby\Toolkit\I18n;
 use Kirby\Toolkit\Str;
 use Kirby\Uuid\Uuids;
@@ -193,11 +194,22 @@ return [
 	'page.changeTitle' => [
 		'pattern' => 'pages/(:any)/changeTitle',
 		'load' => function (string $id) {
-			$request = App::instance()->request();
+			$kirby   = App::instance();
+			$request = $kirby->request();
 
 			$page        = Find::page($id);
 			$permissions = $page->permissions();
 			$select      = $request->get('select', 'title');
+
+			// build the path prefix
+			$path = match ($kirby->multilang()) {
+				true  => Str::after($kirby->site()->url(), $kirby->url()) . '/',
+				false => '/'
+			};
+
+			if ($parent = $page->parent()) {
+				$path .= $parent->uri() . '/';
+			}
 
 			return [
 				'component' => 'k-form-dialog',
@@ -211,7 +223,7 @@ return [
 						'slug' => Field::slug([
 							'required'  => true,
 							'preselect' => $select === 'slug',
-							'path'      => $page->parent() ? '/' . $page->parent()->uri() . '/' : '/',
+							'path'      => $path,
 							'disabled'  => $permissions->can('changeSlug') === false,
 							'wizard'    => [
 								'text'  => I18n::translate('page.changeSlug.fromTitle'),
