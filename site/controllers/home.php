fa10447704941842ee7822125361397feeaf7c24
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-11-16 19:04:52 -0500

    Change layout of recent post home section

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -15,9 +15,34 @@ return function () {
 	$latest_issues = page("numeros")
 		->children()->listed()->flip()->slice(1, 3);
 
+	$recentPosts = pages()->children();
+	$timeAgo = strtotime('-3 months');
+
+	$comptesrendus = page('comptes-rendus')->children()->listed()->sortBy('date', 'desc');
+	$comptesrendus = $comptesrendus->filter(function ($comptesrendu) use ($timeAgo) {
+		return $comptesrendu->date()->toDate() >= strtotime('-3 months');
+	});
+	$recentPosts = $recentPosts->merge($comptesrendus);
+
+	$entretiens = page('entretiens')->children()->listed()->sortBy('date', 'desc');
+	$entretiens = $entretiens->filter(function ($entretien) use ($timeAgo) {
+		return $entretien->date()->toDate() >= $timeAgo;
+		$strtotime('-3 months');
+	});
+	$recentPosts = $recentPosts->merge($entretiens);
+
+	$creations = page('creations')->children()->listed()->sortBy('date', 'desc');
+	$creations = $creations->filter(function ($creation) use ($timeAgo) {
+		return $creation->date()->toDate() >= $timeAgo;
+	});
+	$recentPosts = $recentPosts->merge($creations);
+
+	$recentPosts = $recentPosts->sortBy('date', 'desc');
+
 	return [
 		"last_issue" => $last_issue,
 		"latest_issues" => $latest_issues,
-		"all_news" => $all_news->flip()
+		"all_news" => $all_news->flip(),
+		"recentPosts" => $recentPosts
 	];
 };
--- site/snippets/home/recent-posts.php
+++ site/snippets/home/recent-posts.php
@@ -1,76 +1,43 @@
 <?php
-$recentPosts = [];
-
-// Get the "comptes-rendus" within the last 3 months
-$comptesrendus = page('comptes-rendus')->children()->listed()->sortBy('date', 'desc');
-foreach ($comptesrendus as $comptesrendu) {
-    if ($comptesrendu && $comptesrendu->date()->toDate() >= strtotime('-3 months')) {
-        array_push($recentPosts, $comptesrendu);
-    }
-}
-
-// Get the "entretiens" within the last 3 months
-$entretiens = page('entretiens')->children()->listed()->sortBy('date', 'desc');
-foreach ($entretiens as $entretien) {
-    if ($entretien && $entretien->date()->toDate() >= strtotime('-3 months')) {
-        array_push($recentPosts, $entretien);
-    }
-}
-
-// Get the "creations" within the last 3 months
-$creations = page('creations')->children()->listed()->sortBy('date', 'desc');
-foreach ($creations as $creation) {
-    if ($creation && $creation->date()->toDate() >= strtotime('-3 months')) {
-        array_push($recentPosts, $creation);
-    }
-}
-
 // Display the recent posts if there are any
 if (count($recentPosts) > 0): ?>
-    <section class="home-section last-issue">
-        <h2 class="home-section__title">Publications récentes</h2>
-        <?php foreach ($recentPosts as $p): ?>
-            <ul class="toc">
-                <li class="toc__item" style="--issue-color: <?= $p->parent()->color() ?>">
-                    <div style="display: flex; flex-direction: row;">
+	<section class="home-section recent-posts">
+		<h2 class="home-section__title">Publications récentes</h2>
+		<ul class="toc">
+			<?php foreach ($recentPosts as $p): ?>
+				<li class="toc__item_horizontal" style="--issue-color: <?= $p->parent()->color() ?>">
+						<?php
+						if ($cover = $p->cover()->toFile()): ?>
+							<picture>
+								<img src="<?= $cover->url() ?>" alt="cover">
+							</picture>
+						<?php endif; ?>
 
-                        <!-- <?php if ($cover = $p->cover()->toFile()): ?>
-                            <div style="
-                            background-image: url('<?= $cover->url() ?>');
-                            background-size: cover;
-                            background-position: center;
-                            width: 150px;
-                            height: 100px;
-                            ">
-                            </div>
-                        <?php endif; ?> -->
+						<div>
+							<small>
+								<?= $template = $p->template() == 'review' ?
+									'Compte-rendu' :
+									'Entretien'
+								?>
+							</small>
 
-                        <div>
-                            <small>
-                                <?= $template = $p->template() == 'review' ?
-                                    'Compte-rendu' :
-                                    'Entretien'
-                                    ?>
-                            </small>
+							<a href="<?= $p->url() ?>">
+								<ul class="toc__item-authors-list">
+									<?php
+									foreach ($p->authors()->toStructure() as $author): ?>
+										<li class="toc__item-author"><?= $author->name() ?></li>
+									<?php endforeach ?>
+								</ul>
+								<p class="toc__item-title"><?= $p->title_and_subtitle() ?></p>
+								<p><?= formatDate($p->date()) ?></p>
+							</a>
+					</div>
+				</li>
 
-                            <a href="<?= $p->url() ?>">
-                                <ul class="toc__item-authors-list">
-                                    <?php
-                                    foreach ($p->authors()->toStructure() as $author): ?>
-                                        <li class="toc__item-author"><?= $author->name() ?></li>
-                                    <?php endforeach ?>
-                                </ul>
-                                <p class="toc__item-title"><?= $p->title_and_subtitle() ?></p>
-                                <p><?= formatDate($p->date()) ?></p>
-                            </a>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        <?php endforeach ?>
-    </section>
+	<?php endforeach ?>
+	</section>
 
-    <!-- <article class="home-section last-issue">
+	<!-- <article class="home-section last-issue">
         <h2 class="home-section__title">Recentes publications</h2>
         <ul>
             <?php foreach ($recentPosts as $post): ?>
@@ -78,7 +45,7 @@ if (count($recentPosts) > 0): ?>
                     <a href="<?= $post->url() ?>">
                         <ul>
                             <?php
-                            foreach ($post->authors()->toStructure() as $author): ?>
+							foreach ($post->authors()->toStructure() as $author): ?>
                                 <li><?= $author->name() ?></li>
                             <?php endforeach ?>
                         </ul>
