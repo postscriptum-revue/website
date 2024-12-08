e4bfee764795d29744bcdee33ce8f3b7f4a9a4c8
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-08 15:16:02 -0500

    Scroll to ref in main text when click note
    Modify plugin in, add style and css

--- assets/scripts/footnotes.js
+++ assets/scripts/footnotes.js
@@ -1,5 +1,6 @@
 // TODO: Fix for Safari.
 alignFootnotes();
+navigateBack();
 
 function alignFootnotes() {
 	const fn_refs = document.querySelectorAll(".footnote-ref");
@@ -18,3 +19,24 @@ function getFootnote(ref) {
 	const id = ref.getAttribute("href");
 	return document.querySelector(id);
 }
+
+function navigateBack() {
+	const footnoteBacklinks = document.querySelectorAll('.footnote-list__backlink');
+	
+	for(const link of footnoteBacklinks ){
+		link.addEventListener('click', function(e) {
+			e.preventDefault();
+
+			const targetElement = getFootnote(link);
+			
+			if (targetElement) {
+				targetElement.scrollIntoView({ behavior: 'smooth'});
+				
+				targetElement.classList.add('footnote-ref-highlight');
+				setTimeout(() => {
+					targetElement.classList.remove('footnote-ref-highlight');
+				}, 1500);
+			}
+		});
+	};
+};
