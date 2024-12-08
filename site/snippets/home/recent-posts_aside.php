d3ea6af7f6c8ed2c4fd8a32c2e2327d44e39d554
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-08 14:24:06 -0500

    Add recent post to home aside

---
+++ site/snippets/home/recent-posts_aside.php
@@ -0,0 +1,36 @@
+<?php
+// Display the recent posts if there are any
+if (count($recent_posts) > 0): ?>
+	<section class="recent-posts">
+		<h2 class="aside-section__title">Publications récentes</h2>
+		<ul class="toc">
+			<?php foreach ($recent_posts as $p): ?>
+				<li class="toc__item_horizontal" style="--issue-color: <?= $p->parent()->color() ?>">
+					<?php
+					if ($cover = $p->cover()->toFile()): ?>
+						<picture>
+							<img src="<?= $cover->url() ?>" alt="cover">
+						</picture>
+					<?php endif; ?>
+
+					<div>
+						<p>
+							<?= $p->blueprint()->title() ?>
+						</p>
+						<small><?= formatDate($p->date(), "MMMM yyyy") ?></small>
+
+						<a href="<?= $p->url() ?>">
+							<ul class="toc__item-authors-list">
+								<?php
+								foreach ($p->authors()->toStructure() as $author): ?>
+									<li class="toc__item-author"><?= $author->name() ?></li>
+								<?php endforeach ?>
+							</ul>
+							<p class="toc__item-title"><?= $p->title_and_subtitle() ?></p>
+						</a>
+					</div>
+				</li>
+
+			<?php endforeach ?>
+	</section>
+<?php endif; ?>
