1fc6742e22c7873e677d5498cbeab3ce69bfa36f
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-23 15:01:38 -0500

    Ignore images and quotes for metatag of review

--- site/controllers/review.php
+++ site/controllers/review.php
@@ -13,23 +13,15 @@ return function ($page) {
 
 	//in order, use the following to fill the meta-tag description:
 	//1 - french abstract
-	//2 - the 150 first caracters of the main text
-	//3 - citation if the first paragraphe does not exist or if it is a link
+	//2 - the 150 first caracters of the first paragraphe that is texte (no image, no link, no quote)
 	$meta_description = "";
 	if ($page->abstract_fr() != '') {
 		$meta_description = substr(strip_tags($page->abstract_fr()), 0, 150) . "[...]";
 	} else {
-
-		$first_p = $page->text()->toBlocks()->first();
-
-		if ($first_p) {
-			$first_p_clean = $first_p->text()->kirbytext()->excerpt();
-			$meta_description = substr($first_p_clean, 0, 150) . "[...]";
-		} else {
-			$meta_description = $page->title_and_subtitle() . " Post-Scriptum " . $page->parent()->num() . "(" . $page->fmt_date() . ")";
-		}
+		$first_p = $page->text()->toBlocks()->filterBy('type', 'text')->first();
+		$first_p_clean = $first_p->text()->kirbytext()->excerpt();
+		$meta_description = substr($first_p_clean, 0, 150) . "[...]";
 	}
-
 
 	return [
 		"issue_color" => $issue_color,
