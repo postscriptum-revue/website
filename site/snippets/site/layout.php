170a432ca6aa4ba9de6c3582eaa16ebf7e6d3f4e
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-23 14:41:10 -0500

    Add metatag author and description for interview

--- site/snippets/site/layout.php
+++ site/snippets/site/layout.php
@@ -2,6 +2,14 @@
 // TODO: Still relevant?
 $color = $issue_color ??  $page->color();
 
+if (!isset($meta_authors)) {
+	$meta_authors = "";
+}
+
+if (!isset($meta_description)) {
+	$meta_description = "";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -11,8 +19,9 @@ $color = $issue_color ??  $page->color();
 	<meta charset="UTF-8">
 	<meta
 		name="viewport"
-		content="width=device-width, initial-scale=1.0"
-	>
+		content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="<?= $meta_authors ?>">
+	<meta name="description" content="<?= $meta_description ?>">
 	<link rel="stylesheet" href="/assets/css/style.css">
 	<!-- TODO: Group in one script -->
 	<script src="/assets/scripts/logo.js" defer></script>
@@ -23,7 +32,7 @@ $color = $issue_color ??  $page->color();
 		<?php
 		if ($page->template() == "issue") {
 			echo "PS{$page->num()} | {$page->title()}";
-		} elseif ($page->template() == "article"){
+		} elseif ($page->template() == "article") {
 			echo "PS{$page->parent()->num()} | {$page->title()}";
 		} else {
 			echo "PS | {$page->title()}";
@@ -34,8 +43,7 @@ $color = $issue_color ??  $page->color();
 
 <body
 	style="--issue-color: <?= $color ?>"
-	class="body--template-<?= $page->template() ?>"
->
+	class="body--template-<?= $page->template() ?>">
 	<aside class="site-aside">
 		<button class="site-aside__mobile-button">
 			<span class="site-aside__mobile-button-icon">
