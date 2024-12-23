7276060c6f37e73869b355bbf21f70ef111750ba
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-23 14:59:02 -0500

    Add metatag author and desription for issue

--- site/controllers/issue.php
+++ site/controllers/issue.php
@@ -1,7 +1,26 @@
 <?php
 
 return function ($page) {
+	$meta_authors = [];
+
+	$meta_authors =	preg_replace('/<\/li>\s*<li>/', ' ', $page->credit_editors());
+	$meta_authors = strip_tags($meta_authors);
+
+	$meta_description = "";
+
+	$first_p = $page->intro_text()->toBlocks()->first();
+
+	if ($first_p->citation()->isNotEmpty()) {
+		$first_p = $page->intro_text()->toBlocks()->nth(1);
+	}
+
+	$first_p_clean = $first_p->text()->kirbytext()->excerpt();
+	$meta_description = substr($first_p_clean, 0, 150) . "[...]";
+
+
 	return [
-		"cover" => $page->cover()->toFile()
+		"cover" => $page->cover()->toFile(),
+		"meta_authors" => $meta_authors,
+		"meta_description" => $meta_description
 	];
 };
